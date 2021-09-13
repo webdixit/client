e8c75e1105ce3ce3382f5de2da920321e6c5139e
Vinícius Pereira <vinguima@amazon.com>
2021-09-13 19:38:11 -0300

    chore: clear test files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
 import { FC } from "react";
 
-import { Counter } from "./components/Counter";
-import reactLogo from "~/assets/react-logo.svg";
-import hamburguer from "~/assets/hamburguer.png";
-
-import "./App.css";
-
-const env = process.env.ENV;
-
 export const App: FC = (): JSX.Element => {
-  return (
-    <div>
-      <h1 className="title">WebDixit</h1>
-      <h2>Environment: {env}</h2>
-      <img src={hamburguer} width="100" height="100" />
-      <img src={reactLogo} width="100" height="100" />
-      <Counter />
-    </div>
-  );
+  return <h1>Hello, World!</h1>;
 };
--- src/components/Counter.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const Counter = () => {
-  const [counter, setCounter] = useState<number>(0);
-
-  return (
-    <button onClick={() => setCounter(counter + 1)}>Counter: {counter}</button>
-  );
-};
