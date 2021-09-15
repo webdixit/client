0b1baa8dfde7b552b113a889b36d78e31f00e65b
Vinícius Pereira <vinguima@amazon.com>
2021-09-14 21:23:51 -0300

    feat: create first connection between users

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
-import { FC } from "react";
+import { useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
 
-export const App: FC = (): JSX.Element => {
-  return <h1>Hello, World!</h1>;
+import "./App.css";
+
+export const App = () => {
+  const [socket, setSocket] = useState<Socket>();
+  const [color, setColor] = useState<string>("white");
+
+  const handleButtonClick = () => {
+    socket?.emit("user-click");
+  };
+
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+
+    socket.on("connect", () => {
+      console.log("User connected on server from client!");
+    });
+
+    // socket.emit("change-bg-color", color);
+    socket.on("change-bg-color", (color: string) => {
+      setColor(color);
+    });
+
+    setSocket(socket);
+  }, []);
+
+  if (!socket) {
+    return <></>;
+  }
+
+  return (
+    <main className="app-container" style={{ backgroundColor: color }}>
+      <button className="app-button" onClick={handleButtonClick}>
+        Change bg color!
+      </button>
+    </main>
+  );
 };
